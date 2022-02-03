9fdeb756d987c06b016e9f60a6e26965047626f4
OskinDI <denis_oskin@rambler.ru>
2022-02-03 19:28:24 +0300

    Initial commit

---
+++ src/test/java/ru/oskin_di/StudentsList/StudentsListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oskin_di.StudentsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
